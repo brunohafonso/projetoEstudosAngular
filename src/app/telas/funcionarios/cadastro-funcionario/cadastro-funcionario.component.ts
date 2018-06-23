81f87c4a7c5ad3b59e30884135f906724a6c0d94
luansousa26 <luan2850@gmail.com>
2018-06-23 19:15:21 -0300

    Metodo Save e get Criado

--- src/app/telas/funcionarios/cadastro-funcionario/cadastro-funcionario.component.ts
+++ src/app/telas/funcionarios/cadastro-funcionario/cadastro-funcionario.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Funcionario } from './cadastro-funcionario.model';
+import { FuncionarioService } from './cadastro-funcionario.service';
 
 @Component({
   selector: 'app-cadastro-funcionario',
@@ -7,13 +8,23 @@ import { Funcionario } from './cadastro-funcionario.model';
   styleUrls: ['./cadastro-funcionario.component.css']
 })
 export class CadastroFuncionarioComponent implements OnInit {
-  
+
   funcionario: Funcionario;
-  constructor() {
+  tabelaFuncionarios: any[] = [];
+  constructor(private cadastroService: FuncionarioService) {
     this.funcionario = new Funcionario();
-   }
+  }
 
   ngOnInit() {
   }
-
+  salvar() {
+    this.cadastroService.salvar(this.funcionario).subscribe((funcionarios) => {
+      alert('Funcionário Salvo com Sucesso!');
+    });
+  }
+  getFuncionarios() {
+    this.cadastroService.getFuncionarios().subscribe((tabela: any[]) => {
+      this.tabelaFuncionarios = tabela;
+    });
+  }
 }
